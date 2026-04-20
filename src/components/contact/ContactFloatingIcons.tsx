3d5e01292ef2967ab25405fd0be5456a57a19a99
Magueth <magueth.pastor@mangproduction.com>
2026-04-20 09:38:01 +0200

    Update ContactFloatingIcons.tsx

--- src/components/contact/ContactFloatingIcons.tsx
+++ src/components/contact/ContactFloatingIcons.tsx
@@ -4,7 +4,6 @@ import { useEffect, useRef } from "react";
 import { gsap } from "gsap";
 import { ScrollTrigger } from "gsap/ScrollTrigger";
 
-// Enregistrer ScrollTrigger si côté client
 if (typeof window !== "undefined") {
   gsap.registerPlugin(ScrollTrigger);
 }
